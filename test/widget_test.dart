7eb92e9c87bd19482ec89aa783dadf1dc0c8c0d4
bowook <mbosx316@naver.com>
2023-11-10 23:09:51 +0900

    Current Position Delete Version

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,8 +13,7 @@ import 'package:navermaptest01/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    var currentPosition;
-    await tester.pumpWidget(MyApp(currentPosition: currentPosition));
+    await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
