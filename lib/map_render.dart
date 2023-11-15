6978c18c584d9c6635080825fd44389f2bee15f8
bowook <mbosx316@naver.com>
2023-11-15 22:43:54 +0900

    Visitor Choose Shop

--- lib/map_render.dart
+++ lib/map_render.dart
@@ -1,3 +1,4 @@
+import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
 
@@ -13,23 +14,78 @@ class _ThirdScreenState extends State<ThirdScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          '출발지, 도착지 설정',
-        ),
-      ),
-      body: SafeArea(
-        child: Column(
-          children: [
-            const Spacer(),
-            Center(
-              child: Text(
-                '위도: ${widget.marker.position.latitude}\n경도: ${widget.marker.position.longitude}',
+      body: Column(
+        children: <Widget>[
+          Row(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(left: 30, top: 30),
+                child: const Text(
+                  "실내\n길 찾기.",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 49, 49, 49),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 35),
+              child: ListView(
+                children: [
+                  DropdownSearch<String>(
+                    popupProps: PopupProps.menu(
+                      showSelectedItems: true,
+                      disabledItemFn: (String s) => s.startsWith('I'),
+                    ),
+                    items: const ["1", "2", "3", "4"],
+                    dropdownDecoratorProps: const DropDownDecoratorProps(
+                      dropdownSearchDecoration: InputDecoration(
+                          labelText: "층",
+                          hintText: "층 선택",
+                          labelStyle: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                          hintStyle: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.bold)),
+                    ),
+                    onChanged: print,
+                  ),
+                  DropdownSearch<String>(
+                    popupProps: PopupProps.menu(
+                      showSelectedItems: true,
+                      disabledItemFn: (String s) => s.startsWith('I'),
+                    ),
+                    items: const ["A", "B", "C", "D"],
+                    dropdownDecoratorProps: const DropDownDecoratorProps(
+                      dropdownSearchDecoration: InputDecoration(
+                          labelText: "매장명",
+                          hintText: "건물 내 매장명",
+                          labelStyle: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                          hintStyle: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.bold)),
+                    ),
+                    onChanged: print,
+                  ),
+                ],
               ),
             ),
-            const Spacer(),
-          ],
-        ),
+          ),
+          const Divider(
+            color: Color.fromARGB(255, 170, 170, 170),
+          ),
+          Expanded(
+            flex: 3,
+            child: Image.network(
+              "https://blog.kakaocdn.net/dn/bVZ1G7/btrGjcUxaqJ/klF18vEYyDWi8k1n8pLPOk/img.jpg",
+              fit: BoxFit.fill,
+            ),
+          ),
+        ],
       ),
     );
   }
